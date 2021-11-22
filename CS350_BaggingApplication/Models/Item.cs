199de5e0e8f34bc1acd4a228ff5d5ab492b82689
Dom <monaco014@wcsu.edu>
2021-11-22 17:35:31 -0500

    Item View Model and Database

--- CS350_BaggingApplication/Models/Item.cs
+++ CS350_BaggingApplication/Models/Item.cs
@@ -10,7 +10,9 @@ namespace CS350_BaggingApplication.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Dimensions Dimension { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
         public ContainerType Container { get; set; }
         public int Quantity { get; set; }
         public int Price { get; set; }
---
+++ CS350_BaggingApplication/ViewModels/ItemFormViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CS350_BaggingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CS350_BaggingApplication.ViewModels
+{
+    public class ItemFormViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        [Required]
+        public IEnumerable<ContainerType> Container { get; set; }
+
+        [Required]
+        public int X { get; set; }
+        
+        [Required]
+        public int Y { get; set; }
+        
+        [Required]
+        public int Z { get; set; }
+        
+        [Required]
+        public int Quantity { get; set; }
+        
+        [Required]
+        public int Price { get; set; }
+    }
+}
