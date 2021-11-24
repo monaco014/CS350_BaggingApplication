e3ef3d2eccbec447620ea4e6d3b98d513132ebd4
Brian Sangalli <sangalli002@wcsu.edu>
2021-11-23 21:16:47 -0500

    Created the Bagging Options screen, there is not currently any options in said page, because we have not yet setup the bagging system. will be needed to be added in the future.

---
+++ CS350_BaggingApplication/Controllers/BaggingOptionController.cs
@@ -0,0 +1,51 @@
+﻿using CS350_BaggingApplication.Models;
+using CS350_BaggingApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS350_BaggingApplication.Controllers
+{
+    public class BaggingOptionController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public BaggingOptionController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            var pack = _context.Packaging.ToList();
+
+            var model = new PackagingViewModel()
+            {
+                Packaging = pack
+            };
+
+            _context.Dispose();
+
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var pack = _context.Items.FirstOrDefault(i => i.Id == id);
+
+            if (pack == null)
+                return HttpNotFound();
+
+            return Content(pack.Name);
+        }
+
+    }
+}
---
+++ CS350_BaggingApplication/ViewModels/PackagingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CS350_BaggingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS350_BaggingApplication.ViewModels
+{
+    public class PackagingViewModel
+    {
+        public List<Packaging> Packaging { get; set; }
+    }
+}
